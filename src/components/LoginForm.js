6ad3d8b3c1c81e54bc07671303e827c1f11bcceb
Robin Orheden <orhedenr@gmail.com>
2016-01-27 15:28:49 +0100

    add support for custom login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,80 +2,150 @@ import React from 'react';
 import ReactMixin from 'react-mixin';
 import { History, Link } from 'react-router';
 
-import Context from './../Context';
+import Utils from '../utils';
 import UserActions from '../actions/UserActions';
+
+class DefaultLoginForm extends React.Component {
+  render() {
+    return (
+      <LoginForm {...this.props}>
+        <div className='sp-login-form'>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="form-horizontal">
+                <div className="form-group">
+                  <label htmlFor="spEmail" className="col-xs-12 col-sm-4 control-label">Email</label>
+                  <div className="col-xs-12 col-sm-4">
+                    <input className="form-control" id="spUsername" name="username" placeholder="Username or Email" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="spPassword" className="col-xs-12 col-sm-4 control-label">Password</label>
+                  <div className="col-xs-12 col-sm-4">
+                    <input type="password" className="form-control" id="spPassword" name="password" placeholder="Password" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="col-sm-offset-4 col-sm-4">
+                    <p className="alert alert-danger" spIf="form.error"><span spBind="form.errorMessage" /></p>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                    <Link to="/forgot" className="pull-right">Forgot Password</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LoginForm>
+    );
+  }
+}
 
 @ReactMixin.decorate(History)
 export default class LoginForm extends React.Component {
   state = {
-    username: '',
-    password: '',
-    isProcessing: false,
-    errorMessage: null
+    fields: {
+      username: '',
+      password: '',
+    },
+    errorMessage: null,
+    isFormProcessing: false
   };
 
   onFormSubmit(e) {
     e.preventDefault();
 
-    var self = this;
-
-    var redirectTo = this.props.redirectTo || '/';
-
-    self.setState({ isProcessing: true });
-
-    UserActions.login({
-      username: this.state.username,
-      password: this.state.password
-    }, function (err, result) {
-      self.setState({ isProcessing: false });
-
+    var next = (err, data) => {
       if (err) {
-        self.setState({ errorMessage: err.message });
-      } else {
-        self.history.pushState(null, redirectTo);
+        return this.setState({
+          isFormProcessing: false,
+          errorMessage: err.message
+        });
       }
+
+      // If the user didn't specify any data,
+      // then simply default to what we have in state.
+      data = data || this.state.fields;
+
+      UserActions.login({
+        username: data.username,
+        password: data.password
+      }, (err, result) => {
+        this.setState({ isFormProcessing: false });
+
+        if (err) {
+          this.setState({ errorMessage: err.message });
+        } else {
+          this.history.pushState(null, this.props.redirectTo || '/');
+        }
+      });
+    };
+
+    this.setState({
+      isFormProcessing: true
     });
+
+    if (this.props.onSubmit) {
+      e.data = this.state.fields;
+      this.props.onSubmit(e, next);
+    } else {
+      next(null, this.state.fields);
+    }
   }
 
-  onUsernameChanged(e) {
-    this.setState({ username: e.target.value });
+  _mapFormFieldHandler(element, tryMapField) {
+    if (element.type === 'input' || element.type === 'textarea') {
+      if (element.props.type !== 'submit') {
+        switch(element.props.name) {
+          case 'login':
+          case 'username':
+            tryMapField('username');
+            break;
+          case 'password':
+            tryMapField('password');
+            break;
+        }
+      }
+    }
   }
 
-  onPasswordChanged(e) {
-    this.setState({ password: e.target.value });
+  _spIfHandler(action, element) {
+    var test = null;
+
+    switch (action) {
+      case 'form.processing':
+        test = this.state.isFormProcessing;
+        break;
+      case 'form.error':
+        test = this.state.errorMessage !== null;
+        break;
+    }
+
+    return test;
+  }
+
+  _spBindHandler(action, element) {
+    var element = false;
+
+    switch (action) {
+      case 'form.errorMessage':
+        let className = element.props ? element.props.className : undefined;
+        element = <span className={className}>{this.state.errorMessage}</span>;
+        break;
+    }
+
+    return element;
   }
 
   render() {
-    return (
-      <div className='login'>
-        <div className="row">
-          <div className="col-xs-12">
-            <form className="form-horizontal" onSubmit={this.onFormSubmit.bind(this)}>
-              <div className="form-group">
-                <label htmlFor="spEmail" className="col-xs-12 col-sm-4 control-label">Email</label>
-                <div className="col-xs-12 col-sm-4">
-                  <input className="form-control" id="spUsername" placeholder="Username or Email" onChange={this.onUsernameChanged.bind(this)} />
-                </div>
-              </div>
-              <div className="form-group">
-                <label htmlFor="spPassword" className="col-xs-12 col-sm-4 control-label">Password</label>
-                <div className="col-xs-12 col-sm-4">
-                  <input type="password" className="form-control" id="spPassword" placeholder="Password" onChange={this.onPasswordChanged.bind(this)} />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="col-sm-offset-4 col-sm-4">
-                  { this.state.errorMessage === null ?
-                    null : <p className="text-danger">{this.state.errorMessage}</p>
-                  }
-                  <button type="submit" className="btn btn-primary" disabled={this.state.isProcessing}>Login</button>
-                  <Link to="/forgot" className="pull-right">Forgot Password</Link>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
+    if (this.props.children) {
+      return (
+        <form onSubmit={this.onFormSubmit.bind(this)}>
+          {Utils.makeForm(this, this._mapFormFieldHandler.bind(this), this._spIfHandler.bind(this), this._spBindHandler.bind(this))}
+        </form>
+      );
+    } else {
+      return <DefaultLoginForm {...this.props} />;
+    }
   }
 }
