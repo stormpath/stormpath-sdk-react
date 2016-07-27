e5cb54643364f28015a9347b1989f6cee81823f2
Robin Orheden <orhedenr@gmail.com>
2016-07-27 16:31:34 +0200

    fix so that redirectTo is unchanged even after state change

--- src/components/LogoutLink.js
+++ src/components/LogoutLink.js
@@ -13,11 +13,11 @@ export default class LogoutLink extends React.Component {
     disabled: false
   };
 
-  _performRedirect() {
+  _performRedirect(primaryRedirectTo) {
     var router = context.getRouter();
     var homeRoute = router.getHomeRoute();
     var loginRoute = router.getLoginRoute();
-    var redirectTo = this.props.redirectTo || (homeRoute || {}).path || (loginRoute || {}).path || '/';
+    var redirectTo = primaryRedirectTo || (homeRoute || {}).path || (loginRoute || {}).path || '/';
 
     this.context.router.push(redirectTo);
   }
@@ -25,17 +25,19 @@ export default class LogoutLink extends React.Component {
   onClick(e) {
     e.preventDefault();
 
+    let primaryRedirectTo = this.props.redirectTo;
+
     if (!this.state.disabled) {
       this.setState({ disabled: true });
 
       UserActions.logout(() => {
-        this._performRedirect();
+        this._performRedirect(primaryRedirectTo);
       });
     }
   }
 
   render() {
-    var selectedProps = utils.excludeProps(['href', 'onClick', 'disabled', 'children'], this.props);
+    var selectedProps = utils.excludeProps(['redirectTo', 'href', 'onClick', 'disabled', 'children'], this.props);
 
   	return (
       <a href='#' onClick={this.onClick.bind(this)} disabled={this.state.disabled} {...selectedProps}>
