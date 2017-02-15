7fa84fc74d87ef3c0c93bb9fcc722891a2c2625d
Luka Skukan <luka.skukan@infinum.hr>
2017-02-15 10:39:27 +0100

    Add basic test setup

---
+++ test/helpers/browser.js
@@ -0,0 +1,21 @@
+// Taken from https://semaphoreci.com/community/tutorials/testing-react-components-with-enzyme-and-mocha
+require('babel-register')();
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;
